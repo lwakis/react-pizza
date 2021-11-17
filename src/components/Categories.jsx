b25979734d07da5496da48fe173a8e93978540b0
lwakis <lwakis@yandex.ru>
2021-11-17 07:16:31 +0700

    complete

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,54 +1,35 @@
-import React, {useState} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// class Categories extends React.Component {
-//     state = {
-//         activeItem: 3,
-//     };
-//
-//     onSelectItem = (index) => {
-//     this.setState({
-//         activeItem: index,
-//     });
-// }
-//
-//     render() {
-//         const {items, onClickItem} = this.props;
-//
-//         return (
-//             <div className="categories">
-//                 <ul>
-//                     <li>Все</li>
-//                     {
-//                         items.map((name, index) => (
-//                             <li className={this.state.activeItem === index ? 'active' : ''}
-//                                 onClick={() => this.onSelectItem(index)} key={`${name}_${index}`}>{name}</li>
-//                         ))}
-//                 </ul>
-//             </div>
-//         )
-//     }
-// }
+const Categories = React.memo(function Categories({ activeCategory, items, onClickCategory }) {
+  return (
+    <div className="categories">
+      <ul>
+        <li
+          className={activeCategory === null ? 'active' : ''}
+          onClick={() => onClickCategory(null)}>
+          Все
+        </li>
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={activeCategory === index ? 'active' : ''}
+              onClick={() => onClickCategory(index)}
+              key={`${name}_${index}`}>
+              {name}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+});
 
+Categories.propTypes = {
+  // activeCategory: PropTypes.oneOf([PropTypes.number, null]),
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onClickCategory: PropTypes.func.isRequired,
+};
 
-function Categories({items, onClickItem}) {
-    const [activeItem, setActiveItem] = useState(null);
+Categories.defaultProps = { activeCategory: null, items: [] };
 
-    const onSelectItem = (index) => {
-        setActiveItem(index)
-    }
-
-    return (
-        <div className="categories">
-            <ul>
-                <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>Все</li>
-                {items &&
-                    items.map((name, index) => (
-                        <li className={activeItem === index ? 'active' : ''}
-                            onClick={() => onSelectItem(index)} key={`${name}_${index}`}>{name}</li>
-                    ))}
-            </ul>
-        </div>
-    );
-}
-
-export default Categories
+export default Categories;
